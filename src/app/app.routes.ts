cdd13a8ad2b6a1baf7b7f4935f19b33e1897440f
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-04 12:37:16 +0100

    feat: implement routing and create components for blog application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { CreateBlog } from './create-blog/create-blog';
+import { BlogList } from './blog-list/blog-list';
+import { PostDetail } from './post-detail/post-detail';
+import { ErrorPage } from './error-page/error-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', component: CreateBlog },
+    {path:'blog-list', component: BlogList},
+    {path:'post-detail', component: PostDetail},
+    {path:'**', component:ErrorPage}
+];

c84a5319cbd782a116a89da48d260b5af76dae30
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-06 06:14:31 +0100

    feat: add routes for sign-up, sign-in, and my-blog components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,16 @@ import { CreateBlog } from './create-blog/create-blog';
 import { BlogList } from './blog-list/blog-list';
 import { PostDetail } from './post-detail/post-detail';
 import { ErrorPage } from './error-page/error-page';
+import { SignUp } from './sign-up/sign-up';
+import { SignIn } from './sign-in/sign-in';
+import { MyBlog } from './my-blog/my-blog';
 
 export const routes: Routes = [
     {path:'', component: CreateBlog },
+    {path:'sign-up', component:SignUp},
+    {path: 'sign-in', component: SignIn},
     {path:'blog-list', component: BlogList},
+    {path:'my-blog', component: MyBlog},
     {path:'post-detail', component: PostDetail},
     {path:'**', component: ErrorPage}
 ];

09da5af7d99780d1b182f1b99f389b5b0f9cf7d0
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-10 08:52:19 +0100

    feat: update navigation links in create blog and sign-in components, enhance styles in sign-up component

--- src/app/create-blog/create-blog.ts
+++ src/app/create-blog/create-blog.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterLink } from "@angular/router";
 
 @Component({
   selector: 'app-create-blog',
-  imports: [CommonModule,FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './create-blog.html',
   styleUrl: './create-blog.css'
 })
@@ -12,11 +13,15 @@ export class CreateBlog {
   title = ""
   content = ""
   autoExpand() {
-    const contentArea = document.querySelector('.idea-area .text-editor textarea')as HTMLElement;
+    const contentArea = document.querySelector('.text-editor textarea')as HTMLElement;
     const contentBox = document.querySelector('.content')as HTMLElement;
 
-    contentArea.style.height = 'auto';
+    if(contentArea && contentBox){
+       contentArea.style.height = 'auto';
     contentArea.style.height =contentBox.scrollHeight + 50 + 'px';
+
+    }
+   
   }
   
 

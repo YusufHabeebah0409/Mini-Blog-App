5dbfd674c52f5925c65a16f1098f59747c169848
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-10 10:10:03 +0100

    enhance sign-up component with form binding, validation, and improved layout

--- src/app/sign-up/sign-up.ts
+++ src/app/sign-up/sign-up.ts
@@ -1,12 +1,41 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule, FormsModule],
   templateUrl: './sign-up.html',
   styleUrl: './sign-up.css'
 })
 export class SignUp {
+  userName = ""
+  userEmail = ""
+  userPassword = ""
+  confirmUserPassword = ""
+  userDetails: Array<any> = []
+
+  signUp() {
+    if(this.userName == "" || this.userEmail == "" || this.userPassword == "" || this.confirmUserPassword == ""){
+      alert('All Input feild are required ')
+    }else{
+      this.userDetails.push({ username: this.userName, email: this.userEmail, password: this.userPassword, passwordConfirm: this.confirmUserPassword })
+      localStorage.setItem('userDetails', JSON.stringify(this.userDetails))
+            
+      this.userName = ''
+      this.userEmail = ''
+      this.userPassword = ''
+      this.confirmUserPassword = ''
+
+      window.location.href = '/sign-in'
+
+    }
+
+           
+  }
+
+
+
 
 }

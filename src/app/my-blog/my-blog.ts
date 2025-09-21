c7bdc06a9eea36d61a2c2b65ea21619694fea45b
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-21 11:17:23 +0100

    refactor my-blog component; enhance UI with author display, delete functionality, and improved styling

--- src/app/my-blog/my-blog.ts
+++ src/app/my-blog/my-blog.ts
@@ -1,11 +1,32 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-my-blog',
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './my-blog.html',
   styleUrl: './my-blog.css'
 })
 export class MyBlog {
+   ngOnInit() {
+     const getBlog = localStorage.getItem('blog');
+    if(getBlog){
+      this.createBlog = JSON.parse(getBlog);
+      console.log(getBlog);
+    }
+    
+  }
+
+  createBlog: Array<any> = []
+
+  delNote(index: number) {
+    const removeItem = confirm("Are You Sure You Want To Delete This Note ")
+    if (removeItem === true) {
+      this.createBlog.splice(index, 1)
+      localStorage.setItem('blog', JSON.stringify(this.createBlog))
+    }
+  }
+
 
 }

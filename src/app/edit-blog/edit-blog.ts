0fc68796bf2e85836399047b48a5338c4eca033c
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-22 08:49:42 +0100

    implement edit blog functionality with form setup and local storage integration; enhance UI with styling

--- src/app/edit-blog/edit-blog.ts
+++ src/app/edit-blog/edit-blog.ts
@@ -1,11 +1,55 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-edit-blog',
-  imports: [],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './edit-blog.html',
   styleUrl: './edit-blog.css'
 })
-export class EditBlog {
+export class EditBlog implements OnInit {
+  constructor(private activeRouter: ActivatedRoute) { }
 
+  title = '';
+  content = '';
+  postId: number = -1;
+
+  ngOnInit() {
+    const getBlog = localStorage.getItem('blog');
+    this.postId = Number(this.activeRouter.snapshot.params['id']);
+
+    if (getBlog) {
+      const allBlogs = JSON.parse(getBlog);
+      const post = allBlogs[this.postId];
+      if (post) {
+        this.title = post.title;
+        this.content = post.content;
+      }
+    }
+  }
+
+
+
+
+  editBlog() {
+    const getBlog = localStorage.getItem('blog');
+    if (getBlog) {
+      const allBlogs = JSON.parse(getBlog);
+      allBlogs[this.postId] = {
+        title: this.title,
+        content: this.content
+      };
+
+      localStorage.setItem('blog', JSON.stringify(allBlogs));
+
+      alert('✅ Blog updated successfully!');
+
+      // Redirect back to post detail or blog list
+      // this.router.navigate(['/blog-list']);
+    }
+  }
 }
+
+

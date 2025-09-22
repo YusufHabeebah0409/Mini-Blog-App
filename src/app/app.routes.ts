c6576de32dec3ca6097af0a490ef1e29702dab15
Habeebah <ojuolapehabeebah@gmail.com>
2025-09-22 08:23:39 +0100

    add edit blog functionality with routing and component setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { ErrorPage } from './error-page/error-page';
 import { SignUp } from './sign-up/sign-up';
 import { SignIn } from './sign-in/sign-in';
 import { MyBlog } from './my-blog/my-blog';
+import { EditBlog } from './edit-blog/edit-blog';
 
 export const routes: Routes = [
     {path:'', component: BlogList, title: 'Blog List'},
@@ -14,6 +15,7 @@ export const routes: Routes = [
     {path:'sign-up', component:SignUp, title: 'Sign Up'},
     {path: 'sign-in', component: SignIn, title: 'Sign In'},
     {path:'my-blog', component: MyBlog, title: 'My Blog'},
+    {path:'edit/:id', component:EditBlog, title:'Edit Blog'},
     {path:'post-detail/:id', component: PostDetail, title: 'Post Detail'},
     {path:'**', component: ErrorPage}
 ];
